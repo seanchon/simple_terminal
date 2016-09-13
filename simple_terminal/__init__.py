442135440be7915eacd598db87c15c33fb36b6c0
Sean Chon <schon@gracenote.com>
2016-09-13 10:11:57 -0700

    Refactor code and rebuild tarball.

--- simple_terminal/__init__.py
+++ simple_terminal/__init__.py
@@ -1,11 +1,1 @@
-import subprocess
-
-
-class Terminal(object):
-
-    def command(self, command):
-        proc = subprocess.Popen([command], shell=True, stdout=subprocess.PIPE)
-        output = proc.stdout.readlines()
-        output = map(lambda s: s.strip(), output)
-        proc.kill()
-        return output
+from .terminal import Terminal
---
+++ simple_terminal/terminal.py
@@ -0,0 +1,11 @@
+import subprocess
+
+
+class Terminal(object):
+
+    def command(self, command):
+        proc = subprocess.Popen([command], shell=True, stdout=subprocess.PIPE)
+        output = proc.stdout.readlines()
+        output = map(lambda s: s.strip(), output)
+        proc.kill()
+        return output
