73cd5b16190676c8c16c22198e1a469d3e43c5b0
Sean Chon <schon@gracenote.com>
2016-11-04 22:30:23 -0700

    Add change directory functionality

--- simple_terminal/terminal.py
+++ simple_terminal/terminal.py
@@ -1,11 +1,18 @@
+import os
 import subprocess
 
 
 class Terminal(object):
 
     def command(self, command):
+        if command.split(' ')[0] == 'cd':
+            self.chdir(command.split(' ')[1])
+            output = None
         proc = subprocess.Popen([command], shell=True, stdout=subprocess.PIPE)
         output = proc.stdout.readlines()
         output = map(lambda s: s.strip(), output)
         proc.kill()
         return list(output)
+
+    def chdir(self, directory):
+        os.chdir(directory)
