1c3d2d4ffe21e37c2ff8141512aef5a51df9f54e
Sean Chon <schon@gracenote.com>
2016-11-09 16:13:10 -0800

    Modify self.home_directory to a str for string comparison tests.

--- tests.py
+++ tests.py
@@ -8,7 +8,7 @@ class ChangeDirectoryTestCase(unittest.TestCase):
     def setUp(self):
         self.t = Terminal()
         os.chdir(os.path.expanduser('~'))
-        self.home_directory = os.getcwd()
+        self.home_directory = str(os.getcwd())
 
     def test_current_working_directory(self):
         self.assertEqual(self.t.command('pwd')[0], self.home_directory)
@@ -22,6 +22,12 @@ class ChangeDirectoryTestCase(unittest.TestCase):
         self.assertEqual(self.home_directory, self.t.command('pwd')[0])
 
     def test_change_to_home_directory(self):
+        self.t.command('mkdir -p temp/temp')
+        self.assertEqual(self.home_directory + '/temp/temp', self.t.command('cd temp/temp')[0])
+        self.assertEqual(self.home_directory, self.t.command('cd ~')[0])
+        self.assertEqual(self.home_directory + '/temp/temp', self.t.command('cd ~/temp/temp')[0])
+
+    def test_change_to_relative_directory(self):
         self.t.command('mkdir -p temp/temp')
         self.assertEqual(self.home_directory + '/temp/temp', self.t.command('cd temp/temp')[0])
         self.assertEqual(self.home_directory, self.t.command('cd ~')[0])
