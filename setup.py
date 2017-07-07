33dadcce735e0b260d94b970a84255d07757cae9
Sean Chon <schon@gracenote.com>
2017-07-06 17:03:42 -0700

    Publish 1.0.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.6',
+    version='1.0',
 
     description='A simple way to run terminal commands within a Python script.',
     long_description=long_description,
